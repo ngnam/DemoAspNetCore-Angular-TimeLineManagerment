deb8634ceeaf60fc9821b09af307b60070b44c1f
Nam Nguyen <nguyenvannam0411@gmail.com>
2020-04-25 17:29:09 +0700

    update post-item.component

--- src/TimeLineManager/ClientApp/src/app/post/post-item/post-item.component.ts
+++ src/TimeLineManager/ClientApp/src/app/post/post-item/post-item.component.ts
@@ -8,17 +8,10 @@ import { PostType, Post } from 'src/app/domain/api-models/post-response';
 })
 export class PostItemComponent implements OnInit {
   @Input() item: Post;
-  // tslint:disable-next-line: no-inferrable-types
-  @Input() isRemove: boolean = false;
-  @Output() remoteItem: EventEmitter<{ item: Post }> = new EventEmitter();
 
   postType = PostType;
 
   constructor() {}
 
   ngOnInit(): void {}
-
-  onRemoveItem(item: Post) {
-    this.remoteItem.emit({ item });
-  }
 }
